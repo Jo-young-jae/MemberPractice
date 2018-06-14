7ae6e6aa50f4b1d3be1d57da2a80c0f031b19db4
Unknown <love__65ss@naver.com>
2018-06-14 16:16:13 +0900

    FrontController 로그인 세션 남기기 성공

--- src/kh/web/controller/FrontController.java
+++ src/kh/web/controller/FrontController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kh.web.beans.BoardDAO;
 import kh.web.beans.BoardDTO;
@@ -42,12 +43,14 @@ public class FrontController extends HttpServlet {
 			rd.forward(request, response);
 		}else if(command.equals("/login.do")) {
 			String id = request.getParameter("id");
-			String pw = request.getParameter("pw");			
+			String pw = request.getParameter("pw");
+			HttpSession session = request.getSession();
+			session.setAttribute("loginId", id);
 			boolean result = mem_dao.loginMember(id, pw);			
 			request.setAttribute("result", result);
 			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
 			rd.forward(request, response);
-		}else if(command.equals("/board.do")) {
+		}else if(command.equals("/board.do")) {			
 			List<BoardDTO> result = board_dao.selectBoard();
 			request.setAttribute("result", result);
 			RequestDispatcher rd = request.getRequestDispatcher("boardList.jsp");
