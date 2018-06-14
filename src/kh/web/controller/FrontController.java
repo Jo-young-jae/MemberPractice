ac44e5e13a843a23a2c17f76e90cf20456c6a0d7
Unknown <love__65ss@naver.com>
2018-06-14 15:39:29 +0900

    login.do // sign.do  컨트롤러 수정!
    login은 return 값은 boolean ,  sign return 값은 int

--- src/kh/web/controller/FrontController.java
+++ src/kh/web/controller/FrontController.java
@@ -2,6 +2,7 @@ package kh.web.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +17,9 @@ public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
          
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			
+		
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		
@@ -23,6 +27,29 @@ public class FrontController extends HttpServlet {
 		MembersDAO mem_dao = new MembersDAO();
 		BoardDAO board_dao = new BoardDAO();
 		
+		if(command.equals("/sign.do")) {
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String phone = request.getParameter("phone");
+			
+			int result = mem_dao.signMember(id, pw, name, email, phone);
+			request.setAttribute("result", result);
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			rd.forward(request, response);
+		}else if(command.equals("/login.do")) {
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			boolean result = mem_dao.loginMember(id, pw);
+			request.setAttribute("result", result);
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			rd.forward(request, response);
+		}
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
